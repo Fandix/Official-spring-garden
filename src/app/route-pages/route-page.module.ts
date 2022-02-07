5169747ce7b8e6b70c458689547db81e1e035d17
Fandix Huang <fandix840925@gmail.com>
2022-02-08 00:26:06 +0800

    Change home page design

--- src/app/route-pages/route-page.module.ts
+++ src/app/route-pages/route-page.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@app/material.module';
+import { RouterModule } from '@angular/router';
+
 import { PhoteAlbumComponent } from './phote-album/phote-album.component';
 import { HomeComponent } from './home/home.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FrontPageComponent } from './home/front-page/front-page.component';
 
 @NgModule({
   declarations: [
     PhoteAlbumComponent,
-    HomeComponent
+    HomeComponent,
+    FrontPageComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    RouterModule
   ]
 })
 export class RoutePageModule { }
