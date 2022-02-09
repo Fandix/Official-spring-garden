3a542960895a2752c7f77ec0dc6e90d8c7190e88
fandix.huang <fandix.huang@genie-networks.com>
2022-02-09 09:25:49 +0800

    Change front page

---
+++ src/app/route-pages/home/front-page/front-page.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-front-page',
+  templateUrl: './front-page.component.html',
+  styleUrls: ['./front-page.component.scss']
+})
+export class FrontPageComponent implements OnInit {
+  frontPageLayout: any[] = [
+    { cols: 2, rows: 3, img: "https://scontent.ftpe4-1.fna.fbcdn.net/v/t39.30808-6/272769418_477797507270212_8692923677795669222_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=a26aad&_nc_ohc=U93RarWrur4AX-8rv7C&tn=CejasMduou-WVszK&_nc_ht=scontent.ftpe4-1.fna&oh=00_AT-pzuWbBBtc0auaAx9HVTMvLXxoKnBm5YwGiCxQm5vAhw&oe=6206BE6A" },
+    { cols: 1, rows: 2, img: 'https://www.colorhexa.com/88b06a.png' },
+    { cols: 1, rows: 1, img: 'https://scontent.ftpe7-4.fna.fbcdn.net/v/t39.30808-6/273553728_484715699911726_1424188458320663819_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=a26aad&_nc_ohc=BWi91mRF-CUAX_RenCc&_nc_ht=scontent.ftpe7-4.fna&oh=00_AT9foR5MuqC1t2dTRWqqdVpJPI8fAQifpz1wpmtqtaLIEw&oe=62066E80' },
+    { cols: 1, rows: 1, img: 'https://scontent.ftpe4-1.fna.fbcdn.net/v/t1.6435-9/122665398_190244976025468_5101507419703200180_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=J5vNYkzU6wYAX8_-y01&_nc_ht=scontent.ftpe4-1.fna&oh=00_AT896xQ20R62vR661rk4Qjlqxg9rGmXpe91hG30TAiUO3Q&oe=622503ED' },
+    { cols: 1, rows: 1, img: 'https://scontent.ftpe4-1.fna.fbcdn.net/v/t1.6435-9/126936039_199838998399399_933730028153400397_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=a26aad&_nc_ohc=cWWkKsvL_MAAX_jJQmV&tn=CejasMduou-WVszK&_nc_ht=scontent.ftpe4-1.fna&oh=00_AT9cE-dRCLfYd-4oMqA4vdHUZY020x3J7K7PxQT7nzyFDQ&oe=622813C0' },
+    { cols: 1, rows: 1, img: 'https://www.colorhexa.com/88b06a.png' },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/route-pages/route-page.module.ts
+++ src/app/route-pages/route-page.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@app/material.module';
+import { RouterModule } from '@angular/router';
+
 import { PhoteAlbumComponent } from './phote-album/phote-album.component';
 import { HomeComponent } from './home/home.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from '@shared-component/shared.module';
+import { FrontPageComponent } from './home/front-page/front-page.component';
 @NgModule({
   declarations: [
     PhoteAlbumComponent,
-    HomeComponent
+    HomeComponent,
+    FrontPageComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
     FlexLayoutModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class RoutePageModule { }
